e9ad19f682ec4947e515f080f5c80f9a6052dc21
Niels vd Nieuwendijk <niels.nieuwendijk@gmail.com>
2021-04-21 11:56:26 +0200

    api model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import vehicleStatus
 
-# Register your models here.
+class vehicleStatusAdmin(admin.ModelAdmin):
+    list_display = ('json',)
+    
+admin.site.register(vehicleStatus, vehicleStatusAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class vehicleStatus(models.Model):
+    json = models.TextField()
+
+    def __str__(self):
+        return self.json
+
+    class Meta:
+        verbose_name_plural = "vehicle statusses"
