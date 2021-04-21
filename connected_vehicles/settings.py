95efafebe911a8d9030c0e90ef48fe4e40c0f53c
Niels vd Nieuwendijk <niels.nieuwendijk@gmail.com>
2021-04-21 11:35:48 +0200

    allowed hosts

--- connected_vehicles/settings.py
+++ connected_vehicles/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'ldr&l_ov141*p^%4%wsgekhafqceg57n48&26bfd3og#-279)6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
