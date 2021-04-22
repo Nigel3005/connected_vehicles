06b332079faa74d05f23c8a055ce4d340e4f34ac
Niels vd Nieuwendijk <niels.nieuwendijk@gmail.com>
2021-04-22 11:27:40 +0200

    test

--- overWriteServer.sh
+++ overWriteServer.sh
@@ -1,7 +1,5 @@
 git add .
-git commit -m "autopush"
+read pushname
+git commit -m $pushname
 git push
-ssh -i "/home/niels/Downloads/niels.pem" ubuntu@ec2-15-188-90-156.eu-west-3.compute.amazonaws.com
-cd django
-./overwrite.sh
-exit
+
