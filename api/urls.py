d444bd397e39a6b8ed3056fef5f9a477b2d96bc4
Niels vd Nieuwendijk <niels.nieuwendijk@gmail.com>
2021-04-22 13:42:32 +0200

    apibackk

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from .views import viewapi
 
-# urlpatterns = [
-#     # path('view/', viewapi),
-# ]
+urlpatterns = [
+    path('view/', viewapi),
+]
