6872edb2af1d642d4e81fa3cc99d37bce34ebf6a
Niels vd Nieuwendijk <niels.nieuwendijk@gmail.com>
2021-04-22 14:45:46 +0200

    a

--- api/views.py
+++ api/views.py
@@ -15,7 +15,7 @@ class createVehicleStatus(APIView):
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            json = serializer.data.get('json')
+            json = serializer.data.get('vehicleid')
             vehicle_status = vehicleStatus(json=json)
             vehicle_status.save()
             return Response(viewVehicleStatusSerializer(vehicle_status).data, status=status.HTTP_200_OK)
