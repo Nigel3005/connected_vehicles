3554ee3d686f6e142fe089513526e37d0d208820
Niels vd Nieuwendijk <niels.nieuwendijk@gmail.com>
2021-04-22 13:30:15 +0200

    api

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import viewapi
+
+urlpatterns = [
+    path('view/', viewapi),
+]
--- connected_vehicles/settings.py
+++ connected_vehicles/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,4 +124,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
