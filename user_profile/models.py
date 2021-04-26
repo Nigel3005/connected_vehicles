96a12486c8d781f340e350a38b225e45648f17f7
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-26 16:07:40 +0200

    some-deleted-fields

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,10 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    vehicle_name = models.CharField(max_length=200, null=True)
     vehicle_id = models.CharField(max_length=11, null=True)
-    test_1 = models.CharField(max_length=200, null=True)
-    test_2 = models.CharField(max_length=200, null=True)
 
 
     def __str__(self):
