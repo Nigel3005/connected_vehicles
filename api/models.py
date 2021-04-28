d43f595991f1788359895bfc2a84b6e672b52232
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-28 12:12:50 +0200

    hoi

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class vehicleStatus(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
     vehicle_name = models.CharField(max_length=200)
     vehicleid = models.CharField(max_length=11)
     payload = models.CharField(max_length=200)
