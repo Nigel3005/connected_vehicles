1a6e3b5ebdac58d4908eb93307283d2dbd66da40
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-28 15:21:11 +0200

    hii

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.conf.urls import url
 from . import views
 # from .views import home
 from django.contrib.auth.forms import UserCreationForm
@@ -10,6 +11,6 @@ urlpatterns = [
     path('', views.indexView, name='Home'),
     path('login', LoginView.as_view(), name="Login"),
     path('register', views.registerView, name="Register"),
-    path('account_activation_sent', views.account_activation_sentView, name="Register"),
-    path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activateView, name='Account Activeren'),
+    url('account_activation_sent', views.account_activation_sentView, name="Register"),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activateView, name='Account Activeren'),
 ]
