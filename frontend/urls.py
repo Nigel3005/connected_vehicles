9a830816b3d77160492393f362d876c3888e6d33
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-28 11:06:25 +0200

    register

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,10 +3,11 @@ from . import views
 # from .views import home
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from django.conf.urls import url
 
 urlpatterns = [
     path('status', views.status, name='status'),
     path('', views.indexView, name='Home'),
     path('login', LoginView.as_view(), name="Login"),
-    path('register', LoginView.as_view(), name="Register"),
+    path('register', views.register, name="Register"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,5 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.contrib.sites import requests
-from django.http import HttpResponse
-from rest_framework import viewsets, request
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import generics, status
 from django.shortcuts import render
 from api.models import vehicleStatus
 from django.contrib.auth.decorators import login_required
@@ -22,3 +17,17 @@ def status(request):
     vehicle_statusses = vehicleStatus.objects.filter(vehicleid=vehicleid)
     args = {'vehicle_statusses': vehicle_statusses, 'vehicleid':vehicleid}
     return render(request, template_name, args)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index.html')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
