ffaa2ce78513788c6ff948f21022eb77aa7576a5
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-28 15:26:05 +0200

    hhhaaabdjsisidhusjidshiuschlauscx

--- connected_vehicles/urls.py
+++ connected_vehicles/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import url
+import frontend.views as views
 
 
 urlpatterns = [
@@ -8,6 +10,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('home/', include('frontend.urls')),
     path('', include('frontend.urls')),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activateView, name='Account Activeren'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
