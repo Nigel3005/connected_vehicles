64ce73bc13742d5871c73284da9d37e485e19088
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-28 15:29:37 +0200

    heil;

--- connected_vehicles/urls.py
+++ connected_vehicles/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('home/', include('frontend.urls')),
     path('', include('frontend.urls')),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activateView, name='Account Activeren'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activateView, name='activate'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
