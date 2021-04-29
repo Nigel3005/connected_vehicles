54d694a2547f44bd9b64221e9602da940b1e59a7
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-29 14:13:55 +0200

    ss

--- frontend/urls.py
+++ frontend/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', views.indexView, name='Home'),
     path('login', LoginView.as_view(), name="Login"),
     path('register', views.registerView, name="Register"),
-    path('account-activation-send', views.account_activation_sendView, name="Register"),
+    path('account-activation-send', views.account_activation_sendView, name="activation"),
     path('activate/<slug:uidb64>/<slug:token>/', views.activateView, name='activate'),
 
 ]
