bc0066e608959567878fde73f017ebaaf5771c12
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-29 14:52:36 +0200

    qw

--- connected_vehicles/settings.py
+++ connected_vehicles/settings.py
@@ -137,4 +137,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nigelwerk25@gmail.com'
-EMAIL_HOST_PASSWORD = 'HHBB1122'
+EMAIL_HOST_PASSWORD = 'ymwxplfeyxnapulv'
