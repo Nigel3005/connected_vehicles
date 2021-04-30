07af87fe46eda320e8ddace01d8c93236164eab6
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-30 15:10:49 +0200

    ads

--- frontend/views.py
+++ frontend/views.py
@@ -45,7 +45,7 @@ def statusView(request):
         args = {'page':'status.html', 'vehicle_statusses': vehicle_statusses, 'vehicleid':vehicleid}
         return render(request, 'default.html', args)
     else:
-        args = {'page': 'status.html', 'vehicle_statusses': None, 'vehicleid': None}
+        args = {'page': 'status.html', 'vehicle_statusses': None, 'vehicleid': 'Login Required'}
         return render(request, 'default.html', args)
 
 def registerView(request):
