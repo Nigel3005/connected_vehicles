3c0a11d18ee9a38d5467414518d7c755ce4ca3d0
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-04-30 14:36:40 +0200

    asx

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django import forms
 
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=200)
+    password = forms.CharField(widget=forms.PasswordInput())
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
@@ -11,9 +16,9 @@ class SignUpForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2', )
 
 
-class LoginForm(AuthenticationForm):
-    def pizza(self):
-        print('pizza')
+# class LoginForm(AuthenticationForm):
+#     def pizza(self):
+#         print('pizza')
     # username = forms.CharField(label='Username', max_length=50)
     # password = forms.CharField(label='Password', max_length=50, type='password')
     #
--- frontend/views.py
+++ frontend/views.py
@@ -69,7 +69,7 @@ def registerView(request):
 
 def loginView(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = LoginForm
         if form.is_valid():
             user = authenticate(username=form.username, password=form.password)
             if user is not None:
