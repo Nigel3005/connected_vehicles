46c960b30f983a13f50e11bfe49067fd036230e7
Niels vd Nieuwendijk <niels.nieuwendijk@gmail.com>
2021-04-30 13:20:25 +0200

    auto

--- frontend/forms.py
+++ frontend/forms.py
@@ -8,7 +8,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('usernaame', 'email', 'password1', 'password2', )
 
 
 class LoginForm(AuthenticationForm):
