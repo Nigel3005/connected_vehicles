23f8b921a652fa84266baf3f37027d037386a51a
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-05-26 16:50:21 +0200

    asd

--- api/models.py
+++ api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class vehicleStatus(models.Model):
     time = models.DateTimeField(auto_now_add=True)
-    vehicle_name = models.CharField(max_length=200)
     vehicleid = models.CharField(max_length=11)
     laden = models.CharField(max_length=200)
     cell_spanning = models.CharField(max_length=200)
