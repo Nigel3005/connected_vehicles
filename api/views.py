fbfe05389f00a1962116ab34eb2432c91a63bcd8
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-06-01 09:24:07 +0200

    dsf

--- api/views.py
+++ api/views.py
@@ -17,9 +17,9 @@ class createVehicleStatus(APIView):
             payload = bytes.fromhex(payload_enc).decode('utf-8')
             laden =  payload[0]
             cell_spanning = int(payload[1:2])/100+2
-            accu_spanning = int(payload[3:4])/10
-            cell_percentage = int(payload[5:6])
-            motor_temperatuur = int(payload[7:8])-100
+            accu_spanning = int(payload[3:5])/10
+            cell_percentage = int(payload[6:7])
+            motor_temperatuur = int(payload[8:9])-100
             vehicle_id = request.headers.get('vehicleid').upper()
             vehicle_status = vehicleStatus(vehicle_id=vehicle_id, laden=laden, cell_spanning=cell_spanning, accu_spanning=accu_spanning, cell_percentage=cell_percentage, motor_temperatuur=motor_temperatuur)
             vehicle_status.save()
