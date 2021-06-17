10acf2239e716eff45b94b6eca17f14fbc91492b
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-06-17 16:23:10 +0200

    hi

--- api/views.py
+++ api/views.py
@@ -19,17 +19,17 @@ class createVehicleStatus(APIView):
             alarm_enc = payload[0:3]
             snelheid = int(payload[3:6])/100+2
             trilling = int(payload[6:9])/10
-            vermogen = int(payload[9:12])
-            airco = int(payload[12:15])
-            verwarming = int(payload[15:18])
-            batterij_percentage = int(payload[18:21])
-            batterij_gemiddeld = int(payload[21:24])
-            accu_spanning = int(payload[24:27])
-            batterij_temperatuur = int(payload[27:30])
-            motor_temperatuur = int(payload[30:33])-100
-            controller_temperatuur = int(payload[33:36])
-            actieradius = int(payload[36:39])
-            error = int(payload[39:42])
+            vermogen = int(payload[9:10])
+            airco = int(payload[1:4])
+            verwarming = int(payload[3:6])
+            batterij_percentage = int(payload[6:7])
+            batterij_gemiddeld = int(payload[7:8])
+            accu_spanning = int(payload[8:9])
+            batterij_temperatuur = int(payload[2:3])
+            motor_temperatuur = int(payload[4:5])-100
+            controller_temperatuur = int(payload[2:4])
+            actieradius = int(payload[4:6])
+            error = int(payload[6:8])
 
             # decode
             alarm = '{0:08b}'.format(int(alarm_enc))
